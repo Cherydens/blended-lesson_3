821b4a0305908d5ce8bbdcdbfd9e84c6ea49b0db
Tanyusha7 <swanonlake@gmail.com>
2023-07-22 12:05:42 +0200

    add hook, searchForm

--- src/pages/Country.jsx
+++ src/pages/Country.jsx
@@ -1,14 +1,17 @@
-import { Section, Container, CountryInfo, Loader } from 'components';
+import { Section, Container, CountryInfo, Loader, GoBackBtn } from 'components';
 import { useFetchCountry } from 'hooks';
-import { Navigate } from 'react-router-dom';
+import { Navigate, useLocation } from 'react-router-dom';
 import { routes } from 'routes';
 
 export const Country = () => {
   const { country, error, isLoading } = useFetchCountry();
+  const location = useLocation();
 
+  const goBackLink = location?.state?.from ?? routes.HOME;
   return (
     <Section>
       <Container>
+        <GoBackBtn path={goBackLink}>Back to countries</GoBackBtn>
         {isLoading && <Loader />}
         {error && <Navigate to={routes.HOME} replace />}
         {country && <CountryInfo country={country} />}
