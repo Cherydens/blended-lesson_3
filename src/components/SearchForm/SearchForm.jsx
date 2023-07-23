94a21d93d51532ed447b084df29ea608477e2692
Tanyusha7 <swanonlake@gmail.com>
2023-07-24 00:12:00 +0200

    update SearchForm, CountrySearch

--- src/components/SearchForm/SearchForm.jsx
+++ src/components/SearchForm/SearchForm.jsx
@@ -1,5 +1,6 @@
 import { FiSearch } from 'react-icons/fi';
 import { BtnSearch, Select, SearchFormStyled } from './SearchForm.styled';
+import { useState } from 'react';
 
 const regions = [
   { id: 'africa', value: 'africa', name: 'Africa' },
@@ -9,14 +10,30 @@ const regions = [
   { id: 'oceania', value: 'oceania', name: 'Oceania' },
 ];
 
-export const SearchForm = () => {
+export const SearchForm = ({ onSubmit }) => {
+  const [query, setQuery] = useState('');
+
+  const onHandleChange = ({ target: { value } }) => {
+    setQuery(value);
+  };
+
   return (
-    <SearchFormStyled>
+    <SearchFormStyled
+      onSubmit={e => {
+        e.preventDefault();
+        onSubmit(query);
+      }}
+    >
       <BtnSearch type="submit">
         <FiSearch size="16px" />
       </BtnSearch>
-      <Select aria-label="select" name="region" required>
-        <option selected disabled defaultValue="">
+      <Select
+        aria-label="select"
+        name="region"
+        required
+        onChange={onHandleChange}
+      >
+        <option selected disabled defaultValue={''}>
           Select a region and press enter
         </option>
         {regions &&
--- src/pages/CountrySearch.jsx
+++ src/pages/CountrySearch.jsx
@@ -6,13 +6,20 @@ import {
   Loader,
   CountryList,
 } from 'components';
+import { useFetchByRegion } from 'hooks';
 
 export const CountrySearch = () => {
+  const { countries, isLoading, error, onHandleSubmit } = useFetchByRegion();
+
   return (
     <Section>
       <Container>
-        <h2>CountrySearch</h2>
-
+        <SearchForm onSubmit={onHandleSubmit} />
+        {isLoading && <Loader />}
+        {error && (
+          <Heading textAlign="center">Something went wrong ...</Heading>
+        )}
+        {countries && <CountryList countries={countries} />}
       </Container>
     </Section>
   );
